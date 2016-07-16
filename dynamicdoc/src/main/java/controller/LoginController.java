4bebd8440e3ba8d89512156642e7d711362c3dd8
vkrishnajayanthi <vkrishnajayanthi>
2016-07-16 02:43:44 -0400

    Added Hibernate and got Login to show all User objects from the database

--- dynamicdoc/src/main/java/controller/LoginController.java
+++ dynamicdoc/src/main/java/controller/LoginController.java
@@ -1,17 +1,28 @@
 package controller;
 
+import model.User;
+
+import org.hibernate.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import util.HibernateUtil;
 
 @Controller
+@EnableWebMvc
 public class LoginController {
 
-	@RequestMapping(value="login", method = RequestMethod.GET, produces = "application/json")
-	public @ResponseBody Object Login(String userName, String password) {
+	@RequestMapping(value="login", method = RequestMethod.GET, 
+			produces = "application/json")
+	//@RequestMapping("login")
+	public @ResponseBody Object Login(String userName, String password) {		
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		Object o = session.createCriteria(User.class).list();
 		
-		return userName + " - " + password;
+		return o;
 	}
 }
